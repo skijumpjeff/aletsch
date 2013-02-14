1cb47baa8e4aa927f2934a386b371e3c5eb2cc4f
skijumpjeff <jpp13@cornell.edu>
2013-02-13 20:41:02 -0500

    Minor rewrite of the code.
    Revised SQLite queries and table schemas. Removed the archive_id field
    from the jobs table. Added a query to delete a job that is used if it is
    detected that a job no longer exists on Glacier.
    
    Removed the Glacier class and moved its operation functions to the
    top-level. Fixed bugs that some of them had, and added a new operation
    to get job status(es). Removed the close() function since it seems
    unnecessary.
    
    Switched from using getop to argparse; still working the kinks out of
    this model but overall it's more dependable and flexible. Rewrote the
    logic for handling .boto config file loads and established a clearer
    order of precedence.

--- glacierizer.py
+++ glacierizer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 #
 # =============================================================================
-#  Version: 1.1 (January 31, 2013)
+#  Version: 1.3 (February 13, 2013)
 #  Author: Jeff Puchalski (jpp13@cornell.edu)
 #
 # =============================================================================
@@ -26,219 +26,209 @@
 # SOFTWARE.
 # =============================================================================
 
-"""Glacierizer:
-Interface to Amazon Web Services' Glacier long-term data storage service.
-
-Usage:
-  glacierizer.py [options] <mode>
-  
-Mode:
-  vault   create <vault name>
-  vault   list
-  archive read|write|delete <vault> <file1> [<file2> ...]
-  job     <job_id> [<vault>]
-    
-Options:
-  -c, --config=file   : load credentials from boto configuration file
-  -h, --help          : display this help and exit
-  -v, --verbose       : verbose output
-  -V, --version       : display version number
-  
-Dependencies:
-  boto (>=2.7.0) Interface to Amazon Web Services
-    https://github.com/boto/boto
-"""
-
-version = '1.2'
-verbose = False
-
-import os, os.path, sys, getopt
-import boto, sqlite3
+import os, os.path, sys, argparse
+import boto, sqlite3, json, textwrap
+from boto import connect_glacier
+from boto.glacier.exceptions import UnexpectedHTTPResponseError
 from boto.pyami.config import Config
 
+# Glacier connection
+glacier = None
+
+# Connect to the SQLite database
+db = sqlite3.connect('glacier.db')
+db.row_factory = sqlite3.Row
+cursor = db.cursor()
+
+def init(access_key, secret_key):
+    global glacier
+
+    glacier = connect_glacier(
+        aws_access_key_id = access_key,
+        aws_secret_access_key = secret_key
+    )
+    
+    # Create the jobs table if it doesn't already exist
+    cursor.execute(sql_jobs)
+    db.commit()
+  
 # SQL queries for vault operations
-sql_vault_create = "CREATE TABLE IF NOT EXISTS {0} (filename text, archive_id text)"
-sql_vault_write  = "INSERT INTO {0} VALUES ('{1}','{2}')"
+sql_vault = "CREATE TABLE IF NOT EXISTS {0} (filename text, archive_id text)"
+sql_vault_create  = "INSERT INTO {0} VALUES ('{1}','{2}')"
 sql_vault_read   = "SELECT * FROM {0} WHERE filename = '{1}'"
 sql_vault_delete = "DELETE FROM {0} WHERE filename = '{1}'"
-
+   
 # SQL queries for managing jobs
-sql_create_jobs = "CREATE TABLE IF NOT EXISTS jobs (id text, archive_id text, action text, status_code text, vault_name text)"
-sql_job_write = "INSERT INTO jobs VALUES ('{1}','{2}','{3}','{4}','{5}')"
-sql_job_read = "SELECT * FROM jobs WHERE job_id = '{1}'"
+sql_jobs = "CREATE TABLE IF NOT EXISTS jobs (id text, action text, status_code text, vault_name text)"
+sql_jobs_create = "INSERT INTO jobs VALUES ('{0}','{1}','{2}','{3}')"
+sql_jobs_read = "SELECT * FROM jobs WHERE id LIKE '{0}%'"
+sql_jobs_read_all = "SELECT * FROM jobs"
+sql_jobs_update = "UPDATE jobs SET status_code = '{0}' WHERE id = '{1}'"
+sql_jobs_delete = "DELETE FROM jobs WHERE id = '{0}'"
+   
+def vault(args):
+    if args.action == 'create':
+        vault_create(args.vault_name)
+    elif args.action == 'list':
+        vault_list(args.vault_name)
 
-class Glacier(object):
-    def __init__(self, access_key, secret_key):
-        # Connect to Glacier
-        if access_key and secret_key:
-            self.conn = boto.connect_glacier(
-                aws_access_key_id = access_key,
-                aws_secret_access_key = secret_key
-            )
-        else:
-            self.conn = boto.connect_glacier()
+def vault_create(vault_name):
+    glacier.create_vault(vault_name)
+    cursor.execute(sql_vault.format(vault_name))
+    db.commit()
+    print 'Created vault %s' % args.vault_name
         
-        # Connect to the SQLite database
-        self.db = sqlite3.connect('glacier.db')
-        self.db.row_factory = sqlite3.Row
-        self.cursor = self.db.cursor()
-        self.cursor.execute(sql_create_jobs)
-            
-    def vault_create(self, vault_name):
-        self.conn.create_vault(vault_name)
-        self.cursor.execute(sql_vault_create.format(vault_name))
-        if verbose:
-            print 'Created vault %s' % vault_name
-        
-    def vault_list(self, vault_name):
-        vault = self.conn.get_vault(vault_name)
-        job = vault.retrieve_inventory()
-        self.cursor.execute(sql_job_write.format(
-            job.id, 
-            job.archive_id,
-            job.action,
-            job.status_code,
-            vault_name
-        ))
-        if verbose:
-            print 'Listing vault %s, job is %s' % (vault_name, job.id)
+def vault_list(vault_name):
+    vault = glacier.get_vault(vault_name)
+    job_id = vault.retrieve_inventory()
+    job = vault.get_job(job_id)
+    cursor.execute(sql_jobs_create.format(
+        job.id,  
+        job.action, 
+        job.status_code, 
+        vault_name
+    ))
+    db.commit()
+    print 'Listing vault %s, job is %s' % (vault_name, job.id)
+    print 'Glacier actions take around 4 hours to complete. Please configure notifications on the vault to know when the job is complete and results are available.'
+    
+def archive(args):
+    vault = glacier.get_vault(args.vault_name)
+    
+    if args.action == 'read':
+        archive_read(vault, args.files)
+    elif args.action == 'write':
+        archive_write(vault, args.files)
+    elif args.action == 'delete':
+        archive_delete(vault, args.files)
+    
+def archive_read(vault, files):
+    print 'Not yet implemented'
+    return
+    
+def archive_write(vault, files):
+    for f in files:
+        archive_id = vault.create_archive_from_file(f, description=f)
+        cursor.execute(sql_vault_create.format(vault.name, f, archive_id))
+        db.commit()
+        print 'Wrote %s (%s)' % (f, archive_id)
+    
+def archive_delete(vault, files):
+    for f in files:
+        archive_id = _get_archive_id(vault.name, f)
+        vault.delete_archive(archive_id)
+        cursor.execute(sql_vault_delete.format(vault.name, f))
+        db.commit()
+        print 'Deleted %s (%s)' % (f, archive_id)
 
-    def archive_read(self, vault_name, files):
-        print 'Not yet implemented'
+def job(args):
+    if args.action == 'output':
+        job_output(args.job_id)
+    elif args.action == 'status':
+        job_status(args.job_id)
+        
+def job_output(job_id):
+    cursor.execute(sql_jobs_read.format(job_id))
+    (id,action,status_code,vault_name) = cursor.fetchone()
+    if not vault_name:
+        print >> sys.stderr, 'Could not find vault name for job %s' % job_id
+        sys.exit(2)
+    vault = glacier.get_vault(vault_name)
+    job = vault.get_job(id)
+    
+    try:
+        output = job.get_output()
+    except UnexpectedHTTPResponseError as e:
+        print e.message
         return
-    
-    def archive_write(self, vault_name, files):
-        for f in files:
-            vault = self.conn.get_vault(vault_name)
-            archive_id = vault.create_archive_from_file(f, description=f)
-            self.cursor.execute(sql_vault_write.format(vault_name, f, archive_id))
-            self.db.commit()
-            if verbose: print 'Wrote %s (%s)' % (f, archive_id)
-    
-    def archive_delete(self, vault_name, files):
-        for f in files:
-            vault = self.conn.get_vault(vault_name)
-            archive_id = self._get_archive_id(vault_name, f)
-            vault.delete_archive(archive_id)
-            self.cursor.execute(sql_vault_delete.format(vault_name, f))
-            self.db.commit()
-            if verbose: print 'Deleted %s (%s)' % (f, archive_id)
-
-    def job(self, job_id, vault_name=None):
-        # Use provided vault name
-        if vault_name:
-            result = self.conn.get_job_output(vault_name, job_id)
-        # Look up vault name in jobs database
-        else:
-            self.cursor.execute(sql_job.format(job_id))
-            job = self.cursor.fetchone()
-            if not job:
-                print >> sys.stderr, 'Could not find job for ID %s' % job_id
-                sys.exit(2)
-            result = self.conn.get_job_output(job.vault_name, job_id)
-        print result
+        
+    print json.dumps(output, indent=4, encoding='utf-8')
+        
+def job_status(job_id=None):
+    if job_id:
+        cursor.execute(sql_jobs_read.format(job_id))
+    else:
+        cursor.execute(sql_jobs_read_all)
+    jobs = cursor.fetchall()
+    for (id, action, status_code, vault_name) in jobs:
+        try:
+            vault = glacier.get_vault(vault_name)
+            job = vault.get_job(id)
+            cursor.execute(sql_jobs_update.format(job.status_code, job.id))
+            db.commit()
+            print "Job:\t%s" % job.id
+            print "Action:\t%s" % job.action
+            print "Status:\t%s" % job.status_code
+            print "Vault:\t%s" % vault.name
+            print "----"
+        except:
+            cursor.execute(sql_jobs_delete.format(id))
                  
-    def _get_archive_id(self, vault_name, file_name):
-        self.cursor.execute(sql_vault_read.format(vault_name, file_name))
-        archive_id = self.cursor.fetchone()
-        if not archive_id:
-            print >> sys.stderr, 'Could not find archive ID for file %s in local database.' % file_name
-            sys.exit(2)
-        return archive_id
-    
-    def close():
-        self.conn.close()
-        self.db.close()
-    
+def _get_archive_id(vault_name, file_name):
+    cursor.execute(sql_vault_read.format(vault_name, file_name))
+    archive_id = cursor.fetchone()
+    if not archive_id:
+        print >> sys.stderr, 'Could not find archive ID for file %s in local database.' % file_name
+        sys.exit(2)
+    return archive_id
+
 #------------------------------------------------------------------------------
-
-def show_help():
-    print >> sys.stdout, __doc__,
-
-def show_usage(script_name):
-    print >> sys.stderr, 'Usage: %s [options] <mode>' % script_name
-    usage_modes = """Mode:
-      vault   create <vault name>
-      vault   list
-      archive read|write|delete <vault> <file1> [<file2> ...]
-      job     <job_id> [<vault>]
-    """
-    print >> sys.stderr, usage_modes
-
+    
 def main():
-    script_name = os.path.basename(sys.argv[0])
-    global verbose
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter,
+        description='Interface to Amazon Glacier storage service.',
+        epilog=textwrap.dedent('''\
+            modes:
+                vault   create|list         vault_name
+                archive read|write|delete   vault_name file1 [file2 ...]
+                job     output|status       job_id
+                
+            dependencies:
+                boto (>=2.7.0) https://github.com/boto/boto"
+            '''))
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s 1.3')
+    parser.add_argument('-c', '--config', )
+    subparsers = parser.add_subparsers()
     
-    try:
-        long_opts = ['help', 'config', 'verbose', 'version']
-        opts, args = getopt.gnu_getopt(sys.argv[1:], 'hc:vV', long_opts)
-    except getopt.GetoptError:
-        show_usage(script_name)
-        sys.exit(1)
+    parser_vault = subparsers.add_parser('vault')
+    parser_vault.set_defaults(func=vault)
+    parser_vault.add_argument('action', choices=['create','list'])
+    parser_vault.add_argument('vault_name')
     
-    # Try to read .boto config from user home dir and current dir
-    boto_cfg = None
-    try:
+    parser_archive = subparsers.add_parser('archive')
+    parser_archive.set_defaults(func=archive)
+    parser_archive.add_argument('action', choices=['read','write','delete'])
+    parser_archive.add_argument('vault_name')
+    parser_archive.add_argument('files', nargs=argparse.REMAINDER)
+     
+    parser_job = subparsers.add_parser('job')
+    parser_job.set_defaults(func=job)
+    parser_job.add_argument('action', choices=['output','status'])
+    parser_job.add_argument('job_id', nargs='?', default='', help='Glacier job ID (can be just the first few characters)')
+    
+    args = parser.parse_args()
+    
+    # Try to read .boto configuration from several places (later ones take precedence)
+    try: # user home directory (~/.boto)
         boto_cfg = Config(os.path.join(os.path.expanduser('~'), '.boto'))
+    except: pass
+    try: # current directory (./.boto)
         boto_cfg = Config('.boto')
-    except:
-        pass
-    
-    for opt, arg in opts:
-        if opt in ('-h', '--help'):
-            show_help()
-            sys.exit()
-        elif opt in ('-c', '--config'):
-            boto_cfg = Config(arg)
-        elif opt in ('-v', '--verbose'):
-            verbose = True
-        elif opt in ('-V', '--version'):
-            print script_name, ' version: ', version
-            sys.exit(0)
-    
+    except: pass
+    try: # command line option (--config <file>)
+        if args.config: boto_cfg = Config(args.config)
+    except: pass
+
     # Load the AWS key credentials
     try:
         access_key = boto_cfg.get('Credentials', 'aws_access_key_id')
         secret_key = boto_cfg.get('Credentials', 'aws_secret_access_key')
     except:
         print >> sys.stderr, 'Could not find .boto config file'
-        show_usage(script_name)
-        return
+        sys.exit(1)
          
-    # Connect to Glacier
-    glacier = Glacier(access_key, secret_key)
-    
-    try:
-        target = args[0]
-        
-        if target in ('v', 'vault', 'VAULT'):
-            mode = args[1]
-            vault_name = args[2]
-            if mode in ('c', 'create', 'CREATE'):
-                glacier.vault_create(vault_name)
-            elif mode in ('l', 'list', 'LIST'):
-                glacier.vault_list(vault_name)
-                
-        elif target in ('a', 'archive', 'ARCHIVE'):
-            mode = args[1]
-            vault_name = args[2]
-            files = args[3:]
-            if mode in ('r', 'read', 'READ'):
-                glacier.archive_read(vault_name, files)
-            elif mode in ('w', 'write', 'WRITE'):
-                glacier.archive_write(vault_name, files)
-            elif mode in ('d', 'delete', 'DELETE'):
-                glacier.archive_delete(vault_name, files)
-                
-        elif target in ('j', 'job', 'JOB'):
-            job_id = args[1]
-            vault_name = args[2]
-            glacier.job(job_id, vault_name)
-        
-        else:
-            show_usage(script_name)
-            sys.exit(2)
-    except:
-        pass
-    
+    init(access_key, secret_key)
+    args.func(args)
+
 if __name__ == '__main__':main()
